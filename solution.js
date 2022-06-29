522d31acbc812de2841eab1556e195e9e6aa5a58
Rami <103028944+Rami24t@users.noreply.github.com>
2022-06-29 13:31:01 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,1 +1,24 @@
-// write your code in this file
+// Solution:
+
+console.log("This is an exercise in console logging");
+
+let exercise = "This is the value.";
+
+console.log(exercise);
+
+let firstName = "Rami";
+
+let lastName = 'Al-Saadi';
+
+console.log(firstName, lastName);
+
+let city = "Valley of the Wind";
+let job = "Prince";
+let age = "36";
+
+console.log(firstName + " " + lastName + " is a " + age + " year old " + job + " who lives in "+ city);
+
+let profile = {firstName, lastName, age, job, city};
+console.log(profile);
+
+console.log(profile.firstName + " " + profile.lastName +" is a " + profile.age + " year old "+ profile.job + " who lives in " + profile.city);
